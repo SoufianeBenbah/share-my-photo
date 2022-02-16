5fdd8a03d20efdea51f72f538a585a2aaf2acda0
SoufianeBenbah <soufianebenbah@outlook.com>
2022-02-16 01:56:21 +0100

    CRUD server done, post and get on client side working. TODO : update post and filter by comments.

--- server/server.js
+++ server/server.js
@@ -1,26 +1,8 @@
-const express = require('express');
-const app = express();
-
 const http = require('http');
+const app = require('./app')
+
 const server = http.createServer(app);
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
-const cors = require("cors");
-const postsRoute = require('./routes/posts');
 const PORT = process.env.PORT || 3000;
-
-
-app.use(bodyParser.json())
-app.use(cors());
-app.use('/posts',postsRoute )
-const uri = "mongodb+srv://souf:motdepasse@cluster.aul9k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true },
-  ()=> {console.log('Connected to db');}
-);
-
-app.get('/', ((req, res) => {
-  res.send('<h1>from server</h1>')}))
 
 server.listen(PORT, () => {
   console.log(`server listening on port : ${PORT}`)
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Routes } from "@angular/router";
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { PicturesComponent } from './pictures/pictures.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AddPictureComponent } from './add-picture/add-picture.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import {PostService} from "./services/post.service";
+import { HttpClientModule} from "@angular/common/http";
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from "@angular/forms";
+
+const routes:Routes=[
+  {path: 'add', component: AddPictureComponent},
+  {path: 'gallery', component: GalleryComponent},
+]
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    PicturesComponent
+    AddPictureComponent,
+    GalleryComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    BrowserAnimationsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [PostService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
